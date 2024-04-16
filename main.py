b5bce96cb30f0c194711375070fda8320e94fb2e
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2024-04-16 18:39:44 +0200

    added datasets to run the task on

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 from niapy.algorithms.basic import GeneticAlgorithm
 from niapy.task import Task
 from Clustering import Clustering
-from sklearn.datasets import load_iris
+import numpy as np
+from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler
-from visualization import visualize_clusters
-from kmeans_clustering import perform_kmeans_clustering
+from sklearn.metrics import silhouette_score, homogeneity_score
 
-iris_dataset = load_iris()
-iris_data = iris_dataset.data[:, :2]
-iris_labels = iris_dataset.target
-
-perform_kmeans_clustering(iris_data, n_clusters=3)
-
-scaler = MinMaxScaler()
-normalized_data = scaler.fit_transform(iris_data)
+datasets_list = [
+    ("Iris", datasets.load_iris().data[:, :2], datasets.load_iris().target),
+    ("Wine", datasets.load_wine().data[:, :2], datasets.load_wine().target),
+    ("Noisy Circles", *datasets.make_circles(n_samples=100, factor=0.5, noise=0.05)),
+    ("Noisy Moons", *datasets.make_moons(n_samples=100, noise=0.05)),
+]
 
 num_clusters = 3
 num_features = 2
+population_size = 70
+crossover_probability = 0.8
+mutation_probability = 0.1
+max_evals = 1000
 
-clustering_problem = Clustering(num_clusters=num_clusters, num_features=num_features,
-                                dimension=num_features * num_clusters, instances=normalized_data, lower=0, upper=1)
+algorithm = GeneticAlgorithm(population_size=population_size, crossover_probability=crossover_probability, mutation_probability=mutation_probability)
 
-task = Task(problem=clustering_problem, max_evals=1000)
+for dataset_name, dataset_data, dataset_labels in datasets_list:
+    np.random.seed(30)
+    scaler = MinMaxScaler()
+    normalized_data = scaler.fit_transform(dataset_data)
 
-algorithm = GeneticAlgorithm(population_size=70, crossover_probability=0.8, mutation_probability=0.1)
-best = algorithm.run(task)
+    clustering_problem = Clustering(num_clusters=num_clusters, num_features=num_features,
+                                     dimension=num_features * num_clusters, instances=normalized_data, lower=0, upper=1)
 
-centroids = best[0].reshape(num_clusters, num_features)
+    task = Task(problem=clustering_problem, max_evals=max_evals)
 
-print("Best solution found:", best)
+    best_solution, best_solution_fitness = algorithm.run(task)
 
-visualize_clusters(normalized_data, iris_labels, centroids, num_clusters, "Gručenje z optimiziranimi centroidi")
+    centroids = best_solution.reshape(num_clusters, num_features)
+
+    silhouette = silhouette_score(normalized_data, clustering_problem.cluster_indices)
+    homogeneity = homogeneity_score(dataset_labels, clustering_problem.cluster_indices)
+
+    print("Dataset:", dataset_name)
+    print("Best solution found:", best_solution)
+    print("Best solution fitness:", best_solution_fitness)
+    print("Silhouette score:", silhouette)
+    print("Homogeneity score:", homogeneity)
+    print("\n")
