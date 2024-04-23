33f1134eb98a640f88d16928dab9ae07c49e7ded
Alen Šaruga <66218970+SarugaAlen@users.noreply.github.com>
2024-04-23 19:04:57 +0200

    changed the visualization function and did some code cleanup

--- visualization.py
+++ visualization.py
@@ -6,20 +6,19 @@ def visualize_clusters(data, labels, centroids, num_clusters, title):
     # Define color map based on the number of clusters
     cmap = plt.cm.get_cmap('viridis', num_clusters)
 
-    # Plot data points for each pair of features
-    for i in range(data.shape[1]):
-        for j in range(i + 1, data.shape[1]):
-            plt.scatter(data[:, i], data[:, j], c=labels, cmap=cmap)
+    # Plot data points
+    plt.scatter(data[:, 0], data[:, 1], c=labels, cmap=cmap)
 
-    # Plot cluster centers with circles colored according to the cluster they belong to
+    # Plot cluster centers with the same color as the cluster they belong to
     for cluster_num in range(num_clusters):
         cluster_color = cmap(cluster_num)
-        plt.scatter(centroids[cluster_num, 0], centroids[cluster_num, 1],
-                    s=100, marker='o', c=[cluster_color], edgecolor='black', label=f'Centroid: {cluster_num}')
+        cluster_centroid = centroids[cluster_num]
+        plt.scatter(cluster_centroid[0], cluster_centroid[1], s=100, marker='o',
+                    facecolor=cluster_color, edgecolor='black',
+                    label=f'Centroid: {cluster_num}')
 
     plt.title(title)
-    plt.xlabel('Feature')
-    plt.ylabel('Feature')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
     plt.legend()
     plt.show()
-
